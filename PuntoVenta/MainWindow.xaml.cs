4a83aa2a670986705d57060c202282735b19368a
inndeso <inndeso.mx@gmail.com>
2025-07-28 01:19:49 -0600

    Productos (seccion) prueba / venta (seccion) prueba

--- PuntoVenta/MainWindow.xaml.cs
+++ PuntoVenta/MainWindow.xaml.cs
@@ -1,36 +1,99 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PuntoVenta
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            CargarProductosPapeleria();
+        }
+
+        private BitmapImage CargarImagen(string rutaRelativa)
+        {
+            var imagen = new BitmapImage();
+            imagen.BeginInit();
+            imagen.UriSource = new Uri(rutaRelativa, UriKind.RelativeOrAbsolute);
+            imagen.DecodePixelWidth = 96; // Ajusta según necesidad
+            imagen.CacheOption = BitmapCacheOption.OnLoad;
+            imagen.CreateOptions = BitmapCacheOption.None;
+            imagen.EndInit();
+            imagen.Freeze(); // Opcional pero recomendado para mejor rendimiento
+            return imagen;
+        }
+
+        private void CargarProductosPapeleria()
+        {
+            var productos = new List<Producto>
+            {
+                new Producto {
+                    Nombre = "Lápiz HB",
+                    Precio = 5.50m,
+                    Codigo = "PAP-1001",
+                    Imagen = CargarImagen("pack://application:,,,/Resources/tijeras.png")
+                },
+                new Producto {
+                    Nombre = "Borrador Blanco",
+                    Precio = 8.00m,
+                    Codigo = "PAP-1002",
+                    Imagen = CargarImagen("pack://application:,,,/Resources/borrador.png")
+                },
+                new Producto {
+                    Nombre = "Cuaderno Profesional",
+                    Precio = 45.90m,
+                    Codigo = "PAP-1003",
+                    Imagen = CargarImagen("pack://application:,,,/Resources/cuaderno.png")
+                },
+                new Producto {
+                    Nombre = "Bolígrafo Azul",
+                    Precio = 12.00m,
+                    Codigo = "PAP-1004",
+                    Imagen = CargarImagen("pack://application:,,,/Resources/boligrafo.png")
+                },
+                new Producto {
+                    Nombre = "Resaltador Amarillo",
+                    Precio = 15.75m,
+                    Codigo = "PAP-1005",
+                    Imagen = CargarImagen("pack://application:,,,/Resources/tijeras.png")
+                },
+                new Producto {
+                    Nombre = "Tijeras Escolares",
+                    Precio = 25.50m,
+                    Codigo = "PAP-1006",
+                    Imagen = CargarImagen("pack://application:,,,/Resources/tijeras.png")
+                }
+            };
+
+            ListaProductos.ItemsSource = productos;
         }
 
         private void Venta_Click(object sender, RoutedEventArgs e)
         {
-            // Mostrar el TextBox y darle foco automáticamente
-            CodigoTextBox.Visibility = Visibility.Visible;
+            VistaEscaneo.Visibility = Visibility.Visible;
+            VistaProductos.Visibility = Visibility.Collapsed;
             CodigoTextBox.Focus();
-            CodigoTextBox.Text = string.Empty;
+        }
+
+        private void Productos_Click(object sender, RoutedEventArgs e)
+        {
+            VistaEscaneo.Visibility = Visibility.Collapsed;
+            VistaProductos.Visibility = Visibility.Visible;
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e) => Close();
+        private void MinimizeWindow(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
         }
     }
 }
---
+++ PuntoVenta/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace PuntoVenta
+{
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public string Codigo { get; set; }
+
+        public BitmapImage Imagen { get; set; }
+
+    }
+}
